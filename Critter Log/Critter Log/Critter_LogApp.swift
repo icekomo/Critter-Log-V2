0326e58c238c834da5849f2afee35f585907ddf6
Josh Gdovin <jgg32@drexel.edu>
2023-04-09 22:33:52 -0400

    added project

---
+++ Critter Log/Critter Log/Critter_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Critter_LogApp.swift
+//  Critter Log
+//
+//  Created by Josh Gdovin on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Critter_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
